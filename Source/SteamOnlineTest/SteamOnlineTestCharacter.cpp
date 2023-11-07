4f1c5da3c2a67e88145d3719c7cfeff1da65f154
张东朴 <>
2023-11-07 12:21:51 +0800

    engine commit

--- Source/SteamOnlineTest/SteamOnlineTestCharacter.cpp
+++ Source/SteamOnlineTest/SteamOnlineTestCharacter.cpp
@@ -50,7 +50,7 @@ ASteamOnlineTestCharacter::ASteamOnlineTestCharacter()
 
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
-    IOnlineSubsystem* OnlineSubsystem;
+    // IOnlineSubsystem* OnlineSubsystem;
 }
 
 void ASteamOnlineTestCharacter::BeginPlay()
